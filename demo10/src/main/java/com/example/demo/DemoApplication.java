259eeb36cc2daa5dd34bff95f8f5c291ae6661d4
aoysabala <42373166+aoysabala@users.noreply.github.com>
2019-01-23 21:22:06 +0700

    Add files via upload

---
+++ demo10/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import com.example.demo.entity.*;
+import com.example.demo.repository.*;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner init(ManuRepository manuRepository,
+						   CoffeeTypeRepository coffeetypeRepository,
+						   BakeryTypeRepository bakerytypeRepository,
+						   ManuTypeRepository manutypeRepository) {
+		return args -> {
+
+			Stream.of("-","ร้อน","เย็น","ปั่น").forEach(coffee ->{
+				CoffeeType coffeetype = new CoffeeType();
+				coffeetype.setCoffee(coffee);
+				coffeetypeRepository.save(coffeetype);
+			});
+			coffeetypeRepository.findAll().forEach(System.out::println);
+			Stream.of("-","SANWICH","CAKE&ROLL","MUFFIN").forEach(bakery ->{
+				BakeryType bakerytype = new BakeryType();
+				bakerytype.setBakery(bakery);
+				bakerytypeRepository.save(bakerytype);
+			});
+			bakerytypeRepository.findAll().forEach(System.out::println);
+			Stream.of("Coffee","Bakery").forEach(menu -> {
+				ManuType manutype = new ManuType();
+				manutype.setManut(menu);
+				manutypeRepository.save(manutype);
+			});
+			manutypeRepository.findAll().forEach(System.out::println);
+
+			Manu manu = new Manu();
+			manu.setName("มอลค่า");
+			manu.setPrice(40);
+			manu.setManutype(manutypeRepository.getOne(1L));
+			manu.setCoffeetype(coffeetypeRepository.getOne(2L));
+			manu.setBakerytype(bakerytypeRepository.getOne(1L));
+			manuRepository.save(manu);
+
+
+
+
+		};
+	}
+}
